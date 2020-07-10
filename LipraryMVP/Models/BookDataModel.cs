163f857b383e0ff3ce594998bcda8744a4b1dd7e
hassan424 <nagamahfod@gmail.com>
2020-07-10 15:51:31 +0300

    first upload

---
+++ LipraryMVP/Models/BookDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LipraryMVP.Models
+{
+    class BookDataModel
+    {
+        public int Book_ID { get; set; }
+        public string Book_Name { get; set; }
+        public string Book_Author { get; set; }
+        public int Book_PageNumber { get; set; }
+    }
+}
